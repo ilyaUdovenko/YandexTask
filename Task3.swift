0215406a7f23df1f03a998718f28083b232c03a8
Ilya Udovenko <ilya.udowencko@yandex.ru>
2019-05-21 21:34:10 +0300

    Use array instead dictionary

--- Task3.swift
+++ Task3.swift
@@ -13,24 +13,18 @@ func run(command: String, response: @escaping (String) -> Void, responseQueue: D
 
 func run(commands: [String], response: @escaping ([String]) -> Void, responseQueue: DispatchQueue)  {
   let group = DispatchGroup()
-  var dic = [Int: String]()
+  var result: [String] = Array(repeating: "", count: commands.count)
   
   for (index, command) in commands.enumerated() {
     group.enter()
     DispatchQueue.global().async {
-      run(command: command, response: { (result) in
-        dic[index] = result
+      run(command: command, response: { (res) in
+        result[index] = res
         group.leave()
       }, responseQueue: responseQueue)
     }
   }
   group.notify(queue: responseQueue) {
-    var result: [String] = []
-    for index in 0..<commands.count {
-      if let element = dic[index] {
-        result.append(element)
-      }
-    }
     response(result)
   }
 }
